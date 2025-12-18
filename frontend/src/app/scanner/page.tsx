811b799b3323675747d6f9bdb7743fd4b9c9ee1e
freeppleman <prossenf@yahoo.it>
2025-12-18 13:11:32 +0100

    d

--- frontend/src/app/scanner/page.tsx
+++
@@ -1,9 +0,0 @@
-import MarketScanner from '@/components/MarketScannerv2';
-
-export default function ScannerPage() {
-  return (
-    <main className="min-h-screen bg-gray-900">
-      <MarketScanner />
-    </main>
-  );
-}
